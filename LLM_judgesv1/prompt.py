380d16bd060cecd68c351e615c16a9263306f0a1
Dorothy^_^ <dorothy@Dorothy-debijibendiannao.local>
2026-06-07 14:08:47 +0800

    consolidate the prompts for style transfer

--- LLM_judgesv1/prompt.py
+++ LLM_judgesv1/prompt.py
@@ -1,7 +1,7 @@
 normal_prompt= """Evaluate the quality of a summary written in a popular science style for a scientific article, targeting the high-school students audience. You must evaluate the summary along three dimensions:
 
 1.Style transfer intensity: the degree to which the scientific content has been effectively transformed into a more accessible, popular-science style referring to well-known popular science magazines such as "WIRED" or "National Geographic".
-2.Content preservation: how accurately the summary reflects the original article, including all major claims and findings.
+2.Content preservation: how accurately the summary reflects the original article, including all major claims, methodologies, findings and contributions.
 3.Language Naturalness: the degree to which the summary reads like human-written text.
 
 Each dimension should be rated on a scale from 1 (worst) to 5 (best).
---
+++ generated_prompts/transfer_prompt.py
@@ -0,0 +1,14 @@
+transfer_prompt="""
+You are a professional science popularization writer.
+
+Task:
+Rewrite the following scientific paper content into a clear and accessible popular-science article explanation, targeting the high school students. 
+
+Requirements:
+
+You should avoid equations and heavy jargons and the total length should be 150-200 words. Please consider the following three dimensions when responding. 
+1.Style transfer intensity: the scientific content should been effectively transformed into a more accessible, popular-science style which  referring to well-known popular science magazines such as "WIRED" or "National Geographic".
+2.Content preservation: the summary should reflect the original article, including all major claims, methodologies, findings and contributions.
+3.Language Naturalness: the summary should read like human-written text.
+
+"""
