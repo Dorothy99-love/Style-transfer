c5bafca3b8e1c0828fd53998f8713091ac513143
Dorothy^_^ <dorothy@Dorothy-debijibendiannao.local>
2026-06-13 09:56:05 +0800

    get rid of the extra blank space

--- generated_prompts/transfer_prompt.py
+++ generated_prompts/transfer_prompt.py
@@ -7,7 +7,7 @@ Rewrite the following scientific paper content into a clear and accessible popul
 Requirements:
 
 You should avoid equations and heavy jargons and the total length should be 150-200 words. Please consider the following three dimensions when responding. 
-1.Style transfer intensity: the scientific content should been effectively transformed into a more accessible, popular-science style which  referring to well-known popular science magazines such as "WIRED" or "National Geographic".
+1.Style transfer intensity: the scientific content should been effectively transformed into a more accessible, popular-science style which referring to well-known popular science magazines such as "WIRED" or "National Geographic".
 2.Content preservation: the summary should reflect the original article, including all major claims, methodologies, findings and contributions.
 3.Language Naturalness: the summary should read like human-written text.
 
